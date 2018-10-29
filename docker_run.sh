f9ea093cda5c816ed397d906ff85625673883e28
tuan-l <tuan-l@outlook.com>
2018-10-29 15:51:48 +0700

    Remove qemu support

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,5 @@
 docker_cmd="docker run --net=host --privileged"
 docker_cmd="$docker_cmd -v /dev/bus/usb/:/dev/bus/usb/:ro"
-docker_cmd="$docker_cmd -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static:ro,rslave"
 docker_cmd="$docker_cmd --name ncsdk -i -t rpi-ncsdk:latest /bin/bash"
 
 echo $docker_cmd
